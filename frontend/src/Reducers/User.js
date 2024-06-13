3df83ed6a444fd81f60c6448ca5b9433328e1104
AmitSingh80 <heyamitsingh7@gmail.com>
2024-06-14 02:03:56 +0530

    upadte

--- frontend/src/Reducers/User.js
+++ frontend/src/Reducers/User.js
@@ -1,7 +1,7 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
-  isAuthenticated : true ,{fasle},
+  isAuthenticated : false,
 };
 
 export const userReducer = createReducer(initialState,(builder)=> {
